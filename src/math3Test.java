53fc44a688ef5c957a5d9a053cf815cba2625c2e
Cao Xiaoguo <907915031@qq.com>
2019-11-04 14:04:48 +0800

    test

---
+++ src/math3Test.java
@@ -0,0 +1,104 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.apache.commons.math3.analysis.function.Add;
+import org.apache.commons.math3.analysis.function.Multiply;
+
+class math3Test {
+
+	/*
+	 * 开发者测试(2) 2 
+	 * 错误测试
+	 */	
+	
+	@Test
+	void test_error1() {
+		math3Use.div(0, 2);
+	}
+	
+	@Test
+	void test_error2() {
+		try {
+			math3Use.div(0, 2);
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	/*
+	 * 开发者测试(2) 3
+	 * 基本单元测试
+	 */	
+	
+	// 测试 commons.math3 库类 Add
+	@Test
+	void test_math3_add() {
+		Add add = new Add();
+		assertEquals(3, add.value(1, 2));
+	}
+		
+	// 测试 commons.math3 库类 Multiply
+	@Test
+	void test_math3_mul() {
+		Multiply mul = new Multiply();
+		assertEquals(2, mul.value(1, 2));
+	}
+	
+	// 测试小程序方法 sub
+	@Test
+	void test_math3_sub() {
+		assertEquals(2, sub(3, 1));
+	}
+	
+	// 测试小程序方法 div
+	@Test
+	void test_math3_div() {
+		assertEquals(2, div(6, 3));
+	}
+	
+	
+	/*
+	 * 开发者测试(2) 4 
+	 * 驱动模块、桩模块测试
+	 */	
+	
+	// 对 abs 方法设置驱动模块
+	double driver_abs(double x) {
+		x = abs(x);
+		return x + 1;
+	}
+	
+	@Test
+	void test_abs() {
+		assertEquals(3, driver_abs(2));
+	}
+	
+	
+	// 对 calculate 方法设置桩模块
+	public static double abs(double x) {
+		return x;
+	}
+	
+	public static double add(double x, double y) {
+		return x;
+	}
+	
+	public static double sub(double x, double y) {
+		return x;
+	}
+	
+	public static double mul(double x, double y) {
+		return x;
+	}
+	
+	public static double div(double x, double y) {
+		return x;
+	}
+
+	@Test
+	void test_calculate() {
+		math3Use.calculate(1, 2);
+	}
+	
+}
---
+++ src/math3Use.java
@@ -0,0 +1,61 @@
+import org.apache.commons.math3.analysis.function.Abs;
+import org.apache.commons.math3.analysis.function.Add;
+import org.apache.commons.math3.analysis.function.Divide;
+import org.apache.commons.math3.analysis.function.Multiply;
+import org.apache.commons.math3.analysis.function.Subtract;
+
+public class math3Use {
+	
+	public static double abs(double x) {
+		Abs abs = new Abs();
+		return abs.value(x);
+	}
+	
+	public static double add(double x, double y) {
+		Add add = new Add();
+		return add.value(x, y);
+	}
+	
+	public static double sub(double x, double y) {
+		Subtract sub = new Subtract();
+		return sub.value(x, y);
+	}
+	
+	public static double mul(double x, double y) {
+		Multiply mul = new Multiply();
+		return mul.value(x, y);
+	}
+	
+	public static double div(double x, double y) {
+		if (x == 0) {
+			throw new ArithmeticException("被除数不能为0！");
+		}
+		Divide div = new Divide();
+		return div.value(x, y);
+	}
+	
+	public static double calculate(double x, double y) {
+		double z = 0;
+		if (x <= 0  || y > 3) {
+			z = sub(z, x);
+			z = add(z, y);
+		} else if (x < 2 && y < 0) {
+			// 0 < x < 2 && y < 0
+			y = abs(y);
+			z = add(z, x);
+			z = mul(z, y);
+		} else {
+			// x >= 2 || 0 <= y <= 3
+			z = add(z, x);
+			z = div(z, y);
+		}		
+		return z;
+	}
+	
+	public static void main(String[] args) { 
+		double x = 2, y = 1;
+		double z = calculate(x, y);
+		System.out.println("the result is：" + z);
+	}
+	
+}
